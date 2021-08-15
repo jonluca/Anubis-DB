7ef9e19a9bd107aac0f0bd306f10f4dd836c58a2
JonLuca DeCaro <jdecaro@usc.edu>
2021-08-15 16:11:26 -0400

    feat: better coding and fixed subdomain addition logic

--- app.js
+++ app.js
@@ -1,58 +1,71 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var index = require('./routes/index');
-var app = express();
-var fs = require('fs');
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const index = require("./routes/verifyDomain");
+const app = express();
+const fs = require("fs");
 //Rate limiter
-var RateLimit = require('express-rate-limit');
-const morgan = require('morgan');
-const helmet = require('helmet');
-var limiter = new RateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 1000, // limit each IP to 100 requests per windowMs
-    delayMs: 0 // disable delaying - full speed until the max limit is reached
+const RateLimit = require("express-rate-limit");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const limiter = new RateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 1000, // limit each IP to 100 requests per windowMs
+  delayMs: 0, // disable delaying - full speed until the max limit is reached
 });
-app.enable('trust proxy', true);
+app.enable("trust proxy", true);
 
 // Logging
-const accessLogStream = fs.createWriteStream(path.join(__dirname, './logs/access.log'), {
-    flags: 'a'
-});
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"', {
-    stream: accessLogStream
-}));
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "./logs/access.log"),
+  {
+    flags: "a",
+  }
+);
+app.use(
+  morgan(
+    ':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
+    {
+      stream: accessLogStream,
+    }
+  )
+);
 // Header security
 app.use(helmet());
 //  apply to all requests
 app.use(limiter);
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({extended: false, limit: '50mb', parameterLimit: 50000}));
+app.use(logger("dev"));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+    limit: "50mb",
+    parameterLimit: 50000,
+  })
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', index);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/", index);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
 });
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 module.exports = app;
--- models/domains.js
+++ models/domains.js
@@ -1,14 +1,23 @@
-var mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/admin", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.connect("mongodb://localhost/admin", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 const schema = new mongoose.Schema({
-    domain: {
-        type: String, index: true
-    }, submittedSubdomains: {
-        type: [String], default: []
-    }, validSubdomains: {
-        type: [String], default: []
-    }
+  domain: {
+    type: String,
+    index: true,
+  },
+  submittedSubdomains: {
+    type: [String],
+    default: [],
+  },
+  validSubdomains: {
+    type: [String],
+    default: [],
+  },
 });
 
-module.exports = mongoose.model('Domains', schema);
+module.exports = mongoose.model("Domains", schema);
